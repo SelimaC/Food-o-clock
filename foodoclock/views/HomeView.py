dd6d4e6065f12711d1b2573223fa8c7f5e308a57
SelimaCurci <seliii@hotmail.it>
2019-03-16 13:19:51 +0100

    Add seacrh bar

--- foodoclock/views/HomeView.py
+++ foodoclock/views/HomeView.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
+from django.shortcuts import render_to_response
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 
 @login_required
 def home(request):
-
-    return render(request, '../templates/home.html', {'page': 1})
+    if request.POST:
+        print(request.POST['term'])
+        return render(request, '../templates/home.html', {'page': 1})
+    else:
+        return render(request, '../templates/home.html', {'page': 1})
