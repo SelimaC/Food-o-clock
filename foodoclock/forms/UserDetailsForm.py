d3c0dd2779ff5706ad045867229928d5ec2a63fd
SelimaCurci <seliii@hotmail.it>
2019-03-18 14:02:43 +0100

    User registration form

--- foodoclock/forms/UserDetailsForm.py
+++ foodoclock/forms/UserDetailsForm.py
@@ -4,10 +4,13 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from foodoclock.models.UserDetails import UserDetails
+from foodoclock.models.MealType import MealType
+from foodoclock.models.Diet import Diet
+from foodoclock.models.Cousine import Cousine
 
 class UserCreateForm(UserCreationForm):
-    cousine = forms.CharField(max_length=50)
-    diet = forms.CharField(max_length=50)
+    cousine = forms.ModelChoiceField(label='Cousine', queryset=Cousine.objects.all())
+    diet = forms.ModelChoiceField(label='Diet', queryset=Diet.objects.all())
     country = forms.CharField(max_length=50)
     age = forms.IntegerField()
 
