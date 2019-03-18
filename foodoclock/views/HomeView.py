95068090f6361a8cd8114b949d587d58cb8882d6
SelimaCurci <seliii@hotmail.it>
2019-03-18 16:59:53 +0100

    Nope

--- foodoclock/views/HomeView.py
+++ foodoclock/views/HomeView.py
@@ -1,11 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.shortcuts import render_to_response
-from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.core.paginator import Paginator
 from foodoclock.models.Recipe import Recipe
 from foodoclock.models.MealType import MealType
-from foodoclock.models.Diet import Diet
 from foodoclock.models.Cuisine import Cuisine
 
 @login_required
@@ -13,17 +10,16 @@ def home(request):
 
     recipes= Recipe.objects.all()
     total=len(recipes)
-    sort_options=['Sort by', 'Title', 'Preparation time']
+    sort_options = ['Sort by', 'Title', 'Preparation time']
     for r in recipes:
         r.ingredients_list=r.ingredients.all()
 
     paginator = Paginator(recipes, 10)  # Show 10 contacts per page
 
-    cuisines=Cuisine.objects.all()
-    meals=MealType.objects.all()
+    cuisines = Cuisine.objects.all()
+    meals = MealType.objects.all()
     page = request.GET.get('page')
     rows = paginator.get_page(page)
-
 
     if request.POST:
         term= request.POST['term']
@@ -32,4 +28,5 @@ def home(request):
                        'meals': meals, 'term': term})
 
     else:
-        return render(request, '../templates/home.html', {'page': 1,'rows': rows,'total':total, 'sort':sort_options,'cuisine':cuisines,'meals':meals})
+        return render(request, '../templates/home.html', {'page': 1, 'rows': rows, 'total': total,
+                        'sort': sort_options, 'cuisine': cuisines,'meals': meals})
